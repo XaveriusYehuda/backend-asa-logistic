8b6aa72096cb491464524cc35b1c07e86c993d61
Xaverius Yehuda <xyehuda3@gmail.com>
2026-07-07 01:28:51 +0700

    perbaikan cors

--- index.js
+++ index.js
@@ -18,20 +18,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  // cors({
-  //   // origin: "http://localhost:5173",
-  //   origin: 'https://asalogistic.co.id',
-  //   credentials: true,
-  // })
-  cors()
-);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Mengizinkan semua origin
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+  origin: [
+    "https://asalogistic.co.id",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/finalRFQ", express.static(path.join(__dirname, "finalRFQ")));
